918b0a67fb240c21fef61552fb6ed69fa42d53aa
SHR <shimryta@gmail.com>
2020-10-15 08:59:28 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,6 +12,11 @@
             <ul class="navbar-nav">
                 {{-- タスク作成ページへのリンク --}}
                 <li class="nav-item">{!! link_to_route('task.create','新規タスクの投稿',[],['class' => 'nav-link'])!!}</li>
+                
+                {{-- ユーザ登録ページへのリンク --}}
+                <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                
+                
             </ul>
         </div>
     </nav>
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,8 @@ Route::get('/','TasksController@index');
 
 Route::resource('task','TasksController');
 
+//認証
+Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup','Auth\RegisterController@register')->name('signup.post');
+
+
